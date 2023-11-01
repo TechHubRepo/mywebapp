30172f4a7fcddccb275834afd18b9bc34a065964
Ram Niwash <ram_niwash@outlook.com>
2023-11-01 18:35:17 +0530

    Update spring boot main class

--- src/main/java/com/techhub/mywebapp/MywebappApplication.java
+++ src/main/java/com/techhub/mywebapp/MywebappApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MywebappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("**************************");
+		System.out.println("******** NAMASKAR ********");
+		System.out.println("**************************");
+		System.out.println("*** TECH EDUCATION HUB ***");
+		System.out.println("**************************");		
 		SpringApplication.run(MywebappApplication.class, args);
 	}
-
 }
