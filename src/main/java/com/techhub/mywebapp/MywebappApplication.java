5f74f5cd821423010028895382b576103a1962d0
Ram Niwash <ram_niwash@outlook.com>
2024-02-03 21:16:13 +0530

    added gitignore file

--- src/main/java/com/techhub/mywebapp/MywebappApplication.java
+++ src/main/java/com/techhub/mywebapp/MywebappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MywebappApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		System.out.println("**************************");
 		System.out.println("******** NAMASKAR ********");
 		System.out.println("**************************");
